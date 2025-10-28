dd11b3d5089321369e45d0c5e59a21f81856bbac
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-10-28 16:39:16 +0700

    patch: adjust setup

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from core.log import logger
 
-app = FastAPI(title="OpenAI Integration")
-
-text = "OpenAI Integration: Server running"
+app = FastAPI(title="SQL Agent Core")
+text = "SQL Agent Core: Server running"
 logger.info(text)
 
 @app.get("/")
 async def root():
-    return text
+    return {"message": text}
